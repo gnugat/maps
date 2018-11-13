f7d5d9ea2614d137d3260d12f3ceff1052d10ed0
Loïc Faugeron <faugeron.loic@gmail.com>
2018-11-13 08:36:00 +0000

    GNUworld - 13/11/18

---
+++ gnu-world/13-11-18/config.js
@@ -0,0 +1,61 @@
+var CONFIG = {
+  "maps": {
+    "GNUWorld_daylight": {
+      "defaultView": [
+        0,
+        0,
+        64
+      ],
+      "defaultZoom": 3,
+      "imageFormat": "png",
+      "lastRendered": [
+        1542097996,
+        0,
+        0,
+        0
+      ],
+      "maxZoom": 7,
+      "name": "GNUworld",
+      "renderView": "isometric",
+      "rotations": [
+        0
+      ],
+      "textureSize": 12,
+      "tileSetGroup": "GNUworld_isometric_t1",
+      "tileSize": [
+        384,
+        384
+      ],
+      "world": "GNUworld",
+      "worldName": "GNUworld",
+      "worldSeaLevel": 64
+    }
+  },
+  "mapsOrder": [
+    "GNUWorld_daylight"
+  ],
+  "tileSetGroups": {
+    "GNUworld_isometric_t1": {
+      "maxZoom": 7,
+      "tileOffsets": [
+        [
+          0,
+          0
+        ],
+        [
+          0,
+          0
+        ],
+        [
+          0,
+          0
+        ],
+        [
+          0,
+          0
+        ]
+      ],
+      "tileWidth": 1
+    }
+  }
+};
