885e2230ee90de0536baf2f0729a257452a0edb6
Loïc Faugeron <faugeron.loic@gmail.com>
2018-11-13 08:27:16 +0000

    ClosCraft - 13/11/18

---
+++ clos-craft/13-11-18/config.js
@@ -0,0 +1,61 @@
+var CONFIG = {
+  "maps": {
+    "ClosCraft_daylight": {
+      "defaultView": [
+        0,
+        0,
+        64
+      ],
+      "defaultZoom": 3,
+      "imageFormat": "png",
+      "lastRendered": [
+        1542097348,
+        0,
+        0,
+        0
+      ],
+      "maxZoom": 7,
+      "name": "ClosCraft",
+      "renderView": "isometric",
+      "rotations": [
+        0
+      ],
+      "textureSize": 12,
+      "tileSetGroup": "ClosCraft_isometric_t1",
+      "tileSize": [
+        384,
+        384
+      ],
+      "world": "ClosCraft",
+      "worldName": "ClosCraft",
+      "worldSeaLevel": 64
+    }
+  },
+  "mapsOrder": [
+    "ClosCraft_daylight"
+  ],
+  "tileSetGroups": {
+    "ClosCraft_isometric_t1": {
+      "maxZoom": 7,
+      "tileOffsets": [
+        [
+          0,
+          0
+        ],
+        [
+          0,
+          0
+        ],
+        [
+          0,
+          0
+        ],
+        [
+          0,
+          0
+        ]
+      ],
+      "tileWidth": 1
+    }
+  }
+};
