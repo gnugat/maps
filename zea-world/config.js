9d2d7acfa273fe5de0dd2111c229c5fd4e737017
Loïc Faugeron <faugeron.loic@gmail.com>
2019-03-13 12:59:42 +0000

    Added zea-world

---
+++ zea-world/config.js
@@ -0,0 +1,92 @@
+var CONFIG = {
+  "maps": {
+    "zea-world_blockfinder": {
+      "defaultView": [
+        0,
+        0,
+        64
+      ],
+      "defaultZoom": 3,
+      "imageFormat": "png",
+      "lastRendered": [
+        1552470304,
+        0,
+        0,
+        0
+      ],
+      "maxZoom": 7,
+      "name": "zea-world_blockfinder",
+      "renderView": "isometric",
+      "rotations": [
+        0
+      ],
+      "textureSize": 12,
+      "tileSetGroup": "zea-world_isometric_t1",
+      "tileSize": [
+        384,
+        384
+      ],
+      "world": "zea-world",
+      "worldName": "zea-world",
+      "worldSeaLevel": 64
+    },
+    "zea-world_daylight": {
+      "defaultView": [
+        0,
+        0,
+        64
+      ],
+      "defaultZoom": 3,
+      "imageFormat": "png",
+      "lastRendered": [
+        1552470304,
+        0,
+        0,
+        0
+      ],
+      "maxZoom": 7,
+      "name": "zea-world",
+      "renderView": "isometric",
+      "rotations": [
+        0
+      ],
+      "textureSize": 12,
+      "tileSetGroup": "zea-world_isometric_t1",
+      "tileSize": [
+        384,
+        384
+      ],
+      "world": "zea-world",
+      "worldName": "zea-world",
+      "worldSeaLevel": 64
+    }
+  },
+  "mapsOrder": [
+    "zea-world_daylight",
+    "zea-world_blockfinder"
+  ],
+  "tileSetGroups": {
+    "zea-world_isometric_t1": {
+      "maxZoom": 7,
+      "tileOffsets": [
+        [
+          0,
+          0
+        ],
+        [
+          0,
+          0
+        ],
+        [
+          0,
+          0
+        ],
+        [
+          0,
+          0
+        ]
+      ],
+      "tileWidth": 1
+    }
+  }
+};
