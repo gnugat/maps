5900d58b9fb4cc2e82053b00b962c325d89fbaa1
Loïc Faugeron <faugeron.loic@gmail.com>
2019-03-15 09:53:31 +0000

    Added Epikraft 01/11/11

---
+++ epikraft/01-11-11/config.js
@@ -0,0 +1,92 @@
+var CONFIG = {
+  "maps": {
+    "epikraft_blockfinder": {
+      "defaultView": [
+        0,
+        0,
+        64
+      ],
+      "defaultZoom": 3,
+      "imageFormat": "png",
+      "lastRendered": [
+        1552637154,
+        0,
+        0,
+        0
+      ],
+      "maxZoom": 9,
+      "name": "epikraft_blockfinder",
+      "renderView": "isometric",
+      "rotations": [
+        0
+      ],
+      "textureSize": 12,
+      "tileSetGroup": "epikraft_isometric_t1",
+      "tileSize": [
+        384,
+        384
+      ],
+      "world": "epikraft",
+      "worldName": "epikraft",
+      "worldSeaLevel": 64
+    },
+    "epikraft_daylight": {
+      "defaultView": [
+        0,
+        0,
+        64
+      ],
+      "defaultZoom": 3,
+      "imageFormat": "png",
+      "lastRendered": [
+        1552637154,
+        0,
+        0,
+        0
+      ],
+      "maxZoom": 9,
+      "name": "epikraft",
+      "renderView": "isometric",
+      "rotations": [
+        0
+      ],
+      "textureSize": 12,
+      "tileSetGroup": "epikraft_isometric_t1",
+      "tileSize": [
+        384,
+        384
+      ],
+      "world": "epikraft",
+      "worldName": "epikraft",
+      "worldSeaLevel": 64
+    }
+  },
+  "mapsOrder": [
+    "epikraft_daylight",
+    "epikraft_blockfinder"
+  ],
+  "tileSetGroups": {
+    "epikraft_isometric_t1": {
+      "maxZoom": 9,
+      "tileOffsets": [
+        [
+          0,
+          0
+        ],
+        [
+          0,
+          0
+        ],
+        [
+          0,
+          0
+        ],
+        [
+          0,
+          0
+        ]
+      ],
+      "tileWidth": 1
+    }
+  }
+};
